cdaeda7b785fa73ecbbbcdb8dcb726f241f2fbfa
kokoslolek <karol.kokoszka.123@gmail.com>
2022-09-13 18:06:22 +0200

    kliknięcie

--- javascript/script.js
+++ javascript/script.js
@@ -3,3 +3,13 @@ const h1 = document.querySelector('h1')
 console.log(h1)
 
 h1.innerHTML="TEST"
+
+let count = 0;
+function counter(){_
+    count++;
+    console.log(count)
+}
+//przypisanie wydarzenia do kliknięcia na h1//
+h1.onclick = function(){
+    counter()
+}
