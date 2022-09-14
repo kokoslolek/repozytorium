a864df32876066879929ed85a3306b9bb24e8c27
kokoslolek <karol.kokoszka.123@gmail.com>
2022-09-14 17:13:44 +0200

    maze 1

--- maze/script.js
+++ maze/script.js
@@ -1,5 +1,19 @@
 let vLET = 1;
 var vVar = 1; 
-
-
 console.log(window)
+
+let x = 0
+function test() {
+    const x = 0;
+}
+
+test()
+//console.log(x)
+
+if(2 == 2){
+    let llet = 1;
+    var vvar = 1;
+}
+
+console.let(vvar)
+console.log(llet)
